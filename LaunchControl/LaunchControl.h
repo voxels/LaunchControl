8bad72bb26715bb149c736480da47308cdd90b63
Michael Edgcumbe <voxels@gmail.com>
2018-08-14 13:13:08 -0400

    Initial Commit

---
+++ LaunchControl/LaunchControl.h
@@ -0,0 +1,19 @@
+//
+//  LaunchControl.h
+//  LaunchControl
+//
+//  Created by Michael Edgcumbe on 8/14/18.
+//  Copyright © 2018 Michael Edgcumbe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaunchControl.
+FOUNDATION_EXPORT double LaunchControlVersionNumber;
+
+//! Project version string for LaunchControl.
+FOUNDATION_EXPORT const unsigned char LaunchControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaunchControl/PublicHeader.h>
+
+
